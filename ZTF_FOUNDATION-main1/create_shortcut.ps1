7f45088fe37b69ec7f466ca9aa20a25f83653c21
Sampa-David <davidjosiassampa@gmail.com>
2026-02-27 21:11:26 +0100

    Modification de plus

--- ZTF_FOUNDATION-main1/create_shortcut.ps1
+++
@@ -1,4 +0,0 @@
-$WshShell = New-Object -comObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut("$Home\Desktop\ZTF Foundation.lnk")
-$Shortcut.TargetPath = "http://ztf.local"
-$Shortcut.Save()
